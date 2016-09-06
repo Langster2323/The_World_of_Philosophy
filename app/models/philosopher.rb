6f9bd8d4ce13af8231a4bfb5bdfeaf8823bdc042
Ahkeem Lang <langahkeem@gmail.com>
2016-09-05 21:02:00 -0500

    Created validations

--- app/models/philosopher.rb
+++ app/models/philosopher.rb
@@ -1,2 +1,10 @@
 class Philosopher < ApplicationRecord
+  has_many :upvotes
+  has_many :downvotes
+
+  validates :name, presence: true
+  validates :birthdate, presence: true
+  validates :concentration, presence: true
+  validates :origin, presence: true
+  validates :wikipedia, presence: true
 end
