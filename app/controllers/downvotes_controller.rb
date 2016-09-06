ee99eda49b973c72310ba9c49426ff697fe7d349
Ahkeem Lang <langahkeem@gmail.com>
2016-09-05 20:50:39 -0500

    Created crud actions for downvote controller

--- app/controllers/downvotes_controller.rb
+++ app/controllers/downvotes_controller.rb
@@ -1,2 +1,24 @@
 class DownvotesController < ApplicationController
+  #Look over this again...
+  def create
+    downvote = Downvote.new(downvote_params)
+    if downvote.save
+      redirect_to :back
+    else
+      render :new
+    end
+
+    def destroy
+      if downvote.delete
+        redirect_to :back
+      else
+        flash[:alert] = "That's odd... like couldn't be placed."
+      end
+    end
+  end
+
+  private
+  def upvote_params
+    params.require(:downvote)
+  end
 end
