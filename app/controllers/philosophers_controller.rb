13db63d86f5338d2ba60636abe24e87d6a390982
Ahkeem Lang <langahkeem@gmail.com>
2016-09-05 20:37:35 -0500

    created crud actions for upvotes and philosopher controllers

--- app/controllers/philosophers_controller.rb
+++ app/controllers/philosophers_controller.rb
@@ -1,2 +1,15 @@
 class PhilosophersController < ApplicationController
+  def index
+    render locals: {
+      philosophers: Philosopher.all
+    }
+  end
+
+  def show
+    if Philosopher.exists?(params[:id])
+      render locals: { philosopher: philosopher.find(params[:id])}
+    else
+      render html: "Not Found", status: 404
+    end
+  end
 end
--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,2 +1,24 @@
 class UpvotesController < ApplicationController
+  #Look over this again...
+  def create
+    upvote = Upvote.new(upvote_params)
+    if upvote.save
+      redirect_to :back
+    else
+      render :new
+    end
+
+    def destroy
+      if upvote.delete
+        redirect_to :back
+      else
+        flash[:alert] = "That's odd... like couldn't be placed."
+      end
+    end
+  end
+
+  private
+  def upvote_params
+    params.require(:upvote)
+  end
 end
