53ac37fa4bb81da1590c63debe52a389e022ac5f
Ahkeem Lang <langahkeem@gmail.com>
2016-09-05 20:54:40 -0500

    created crud actions for work controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,15 @@
 class WorksController < ApplicationController
+  def index
+    render locals: {
+      works: Work.all
+    }
+  end
+
+  def show
+    if Work.exists?(params[:id])
+      render locals: { work: work.find(params[:id])}
+    else
+      render html: "Not Found", status: 404
+    end
+  end
 end
