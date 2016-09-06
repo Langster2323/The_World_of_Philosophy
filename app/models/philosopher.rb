90a51bb8c5435d08273798587fc32c4dfc84cc3a
Ahkeem Lang <langahkeem@gmail.com>
2016-09-06 12:58:31 -0500

    Added a belongs to within the work model, I also added a has many to my philosopher model

--- app/models/philosopher.rb
+++ app/models/philosopher.rb
@@ -1,6 +1,7 @@
 class Philosopher < ApplicationRecord
   has_many :upvotes
   has_many :downvotes
+  has_many :works
 
   validates :name, presence: true
   validates :birthdate, presence: true
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,6 @@
 class Work < ApplicationRecord
+  belongs_to :philosopher
+  
   validates :name, presence: true
   validates :date, presence: true
   validates :form, presence: true
