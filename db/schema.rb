f6e16fbf17836d044d134a7d7da78ebc28edd1f7
Ahkeem Lang <langahkeem@gmail.com>
2016-09-07 17:15:26 -0500

    Migrated new columns within the philosopher table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905200919) do
+ActiveRecord::Schema.define(version: 20160907221342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(version: 20160905200919) do
     t.string   "wikipedia"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "upvotes", force: :cascade do |t|
