f254c16881b2e01952ba50a5e1965f5da0176142
Ahkeem Lang <langahkeem@gmail.com>
2016-09-12 16:09:01 -0500

    Added JavaScript dependencies and worked through clearance steps.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :philosophers
   resources :users
   resources :works
+
+  root 'philosophers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
