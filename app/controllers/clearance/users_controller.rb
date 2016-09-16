d1c64289ca6da8130c182adc1f823015f606c3c5
Ahkeem Lang <langahkeem@gmail.com>
2016-09-15 20:26:18 -0500

    still trying to login into my app

--- app/controllers/clearance/users_controller.rb
+++ app/controllers/clearance/users_controller.rb
@@ -9,47 +9,82 @@ class Clearance::UsersController < Clearance::BaseController
     skip_before_filter :authorize, only: [:create, :new], raise: false
   end
 
+  def index
+    users = User.all.order(:name)
+    render template: 'users/index.html.erb', locals: { users: users }
+  end
+
+  def show
+    user = User.find(params.fetch(:id))
+    if user
+      render template: 'users/show.html.erb', locals: { user: user }
+    else
+      redirect_to users
+    end
+  end
+
   def new
-    @user = user_from_params
-    render template: "users/new"
+    if signed_in?
+      redirect_to "/users"
+    else
+      @user = user_from_params
+      render template: "users/new.html.erb"
+    end
   end
 
   def create
     @user = user_from_params
-
     if @user.save
       sign_in @user
-      redirect_back_or url_after_create
+      redirect_to root_path
     else
-      render template: "users/new"
+      flash[:alert] = "Errors: Unable to create Account."
+      render template: "users/new.html.erb"
+    end
+  end
+
+  def edit
+    render locals: { user: User.find(params.fetch(:id)) }
+  end
+
+  def update
+    @user = user_from_params
+    if @user.save
+      redirect_to user
+    else
+      render template: 'user/edit.html.erb', locals: { user: user }
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params.fetch(:id))
+    if user.destroy
+      render message: "User deleted."
+      redirect_to users_path
+    else
+      render message: "User not found."
     end
   end
 
   private
 
-  def avoid_sign_in
-    warn "[DEPRECATION] Clearance's `avoid_sign_in` before_filter is " +
-      "deprecated. Use `redirect_signed_in_users` instead. " +
-      "Be sure to update any instances of `skip_before_filter :avoid_sign_in`" +
-      " or `skip_before_action :avoid_sign_in` as well"
-    redirect_signed_in_users
-  end
-
   def redirect_signed_in_users
     if signed_in?
-      redirect_to Clearance.configuration.redirect_url
+      redirect_to "/users"
     end
   end
 
   def url_after_create
-    Clearance.configuration.redirect_url
+    redirect_to "/users"
   end
 
   def user_from_params
+    name = user_params.delete(:name)
     email = user_params.delete(:email)
     password = user_params.delete(:password)
 
     Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.name = name
       user.email = email
       user.password = password
     end
